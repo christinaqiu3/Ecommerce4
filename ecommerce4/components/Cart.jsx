50aff8f7384e4012fd8b5d5f9bd21d9aacb0343f
christinaqiu3 <christinaqiu3@gmail.com>
2023-09-02 22:13:56 -0400

    Pretty Much done

--- ecommerce4/components/Cart.jsx
+++ ecommerce4/components/Cart.jsx
@@ -98,9 +98,9 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick={handleCheckout}>
+              {/*<button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
-              </button>
+        </button>*/}
             </div>
           </div>
         )}
--- ecommerce4/components/Navbar.jsx
+++ ecommerce4/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Christina Qiu Website</Link>
+        <Link href="/">mm NFTS yes</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
